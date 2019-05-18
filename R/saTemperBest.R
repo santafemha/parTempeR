2c49cf615e82949e6271bc138d52693fec15c709
Michael Holton Price <MichaelHoltonPrice@gmail.com>
2019-05-18 16:17:46 -0600

    Improved documentation of saTemperBest

--- R/saTemperBest.R
+++ R/saTemperBest.R
@@ -1,5 +1,12 @@
 #' @title Get best value from call to saTemper
 #'
+#' @description Get the best value (the X with the lowest associated value of the cost function) from the output of saTemper. The optional input chainsOnly [default FALSE] allows a set of chains to be passed to saTemperBest rather than the output of saTemper and is used internally by saTemper.
+#'
+#' @param input The output of saTemper or a list of chains, chainList
+#' @param chainsOnly An optional boolean indicating whether the input is a list of chains only [default FALSE]
+#'
+#' @return The parameter X with the lowest value of the cost function
+#'
 #' @author Michael Holton Price <MichaelHoltonPrice@@gmail.com>
 #'
 #' @export
